58bf89659920153201f6e17d1870c843631a7b3f
Yvi-compt <yvesplacide@gmail.com>
2025-01-31 18:14:14 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,8 +1,8 @@
 resource "azurerm_resource_group" "mcit420zz5um" {
-  name     = "mcit_resource_group_w24"
+  name     = "yvesP74"
   location = "canadacentral"
 }
 resource "azurerm_resource_group" "mcit420zz5um2" {
-  name     = "mcit_resource_group_w24_2"
+  name     = "yvesP74_2"
   location = "canadacentral"
 }
