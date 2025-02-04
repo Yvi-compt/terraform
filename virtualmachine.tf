c784795b1d9d4dfb4a9eaff41d2eb8fd0fa7f2d4
Yvi-compt <yvesplacide@gmail.com>
2025-02-03 20:45:31 -0500

    Update virtualmachine.tf

--- virtualmachine.tf
+++ virtualmachine.tf
@@ -11,7 +11,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subneta" {
-  name                 = "subnet-a"
+  name                 = "internal"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["192.168.2.0/24"]
@@ -23,7 +23,7 @@ resource "azurerm_network_interface" "netinterface" {
   resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "subnet-a"
+    name                          = "internal"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
   }
